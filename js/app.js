0d3369f32e9eb22351fac95387e8bb81105a048c
Zychol56 <zychol56@gmail.com>
2021-10-19 21:52:11 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -40,16 +40,19 @@ navigator.geolocation.getCurrentPosition(succes,errors);
             .then(data =>{
                     for(i=0; i < data.results.length; i++){
                         if(data.results.length != 0){
-                    latOpt[i].style.display = "block";
+                            return function (){
                     latOpt[i].textContent = data.results[i].formatted;
+                    latOpt[i].style.display = "block";
                     latOpt[i].addEventListener("click", ((j) => {
+                        return function() {
                             lat = data.results[j].geometry.lat
                             long = data.results[j].geometry.lng
                             noLat.style.display = "none";
                             citydesc.style.display = "none";
                             mainCont.style.display = "block";
-                            main();            
-                      })(i))
+                            main();
+                        }
+                      })(i))}
                     }
                     else{
                         latOpt[i].style.display = "none";
